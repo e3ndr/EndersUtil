2fe4a27a20d26c38eb2a11f3e7c503747f8f0259
Ender <33337309+e3ndr@users.noreply.github.com>
2020-05-30 20:17:55 -0500

    1.3.0
    Async/Await

---
+++ src/main/java/xyz/e3ndr/EndersUtil/concurrent/AsyncCallable.java
@@ -0,0 +1,37 @@
+/**
+ * 2020 e3ndr.
+ * Proudly licensed under MIT. (Don't be a dick though)
+ */
+package xyz.e3ndr.EndersUtil.concurrent;
+
+import java.util.concurrent.Callable;
+
+import lombok.SneakyThrows;
+
+public class AsyncCallable<T> extends Async<T> {
+    private boolean set = false;
+    private T value;
+    
+    public AsyncCallable(Callable<T> callable) {
+        new Thread(() -> this.run(callable)).start();
+    }
+
+    @SneakyThrows
+    private void run(Callable<T> callable) {
+        this.value = callable.call();
+        this.set = true;
+        
+        synchronized (this) {
+            this.notifyAll();
+        }
+    }
+
+    @SneakyThrows
+    @Override
+    public synchronized T await() {
+        if (!this.set) this.wait();
+        
+        return this.value;
+    }
+
+}
---
+++ src/main/java/xyz/e3ndr/EndersUtil/concurrent/AsyncRunnable.java
@@ -0,0 +1,37 @@
+/**
+ * 2020 e3ndr.
+ * Proudly licensed under MIT. (Don't be a dick though)
+ */
+package xyz.e3ndr.EndersUtil.concurrent;
+
+import lombok.SneakyThrows;
+
+/**
+ * An Async class which will return null on completion.
+ */
+public class AsyncRunnable<T> extends Async<T> {
+    private boolean set = false;
+    
+    public AsyncRunnable(Runnable runnable) {
+        new Thread(() -> this.run(runnable)).start();
+    }
+
+    private void run(Runnable runnable) {
+        runnable.run();
+        
+        this.set = true;
+        
+        synchronized (this) {
+            this.notifyAll();
+        }
+    }
+
+    @SneakyThrows
+    @Override
+    public synchronized T await() {
+        if (!this.set) this.wait();
+        
+        return null;
+    }
+
+}
